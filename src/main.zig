656826ad4980af408a18e8afa7b08c7c982f0ed7
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-08-09 09:19:07 +0400

    Add logger and waiting for os signal

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,139 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const time = @cImport(@cInclude("time.h"));
+
+// Init scoped logger.
+const log = std.log.scoped(.main);
+
+// Init our custom logger handler.
+pub const std_options = .{
+    .log_level = .debug,
+    .logFn = logFn,
+};
+
+var wait_signal = true;
+var wait_signal_mutex = std.Thread.Mutex{};
+var wait_signal_cond = std.Thread.Condition{};
+
+fn handleSignal(
+    signal: i32,
+    _: *const std.posix.siginfo_t,
+    _: ?*anyopaque,
+) callconv(.C) void {
+    log.debug("os signal received: {d}", .{signal});
+    wait_signal_mutex.lock();
+    defer wait_signal_mutex.unlock();
+    wait_signal = false;
+    wait_signal_cond.broadcast();
+}
 
 pub fn main() !void {
-    std.debug.print("ttun\n", .{});
+    switch (builtin.os.tag) {
+        .macos, .linux => {},
+        else => {
+            log.err(
+                "ttun can only work on Linux or macOS",
+                .{},
+            );
+            return;
+        },
+    }
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{
+        .safety = true,
+        .thread_safe = true,
+        .verbose_log = true,
+    }){};
+    _ = gpa.allocator();
+    defer std.debug.assert(gpa.deinit() == .ok);
+
+    var act = std.posix.Sigaction{
+        .handler = .{ .sigaction = handleSignal },
+        .mask = std.posix.empty_sigset,
+        .flags = (std.posix.SA.SIGINFO),
+    };
+    var oact: std.posix.Sigaction = undefined;
+    try std.posix.sigaction(std.posix.SIG.INT, &act, &oact);
+    waitSignalLoop();
+
+    log.info("successfully exiting...", .{});
+}
+
+fn waitSignalLoop() void {
+    log.info("starting to wait for os signal", .{});
+    _ = shouldWait(0);
+    log.info("exiting os signal waiting loop", .{});
+}
+
+fn shouldWait(ms: u64) bool {
+    wait_signal_mutex.lock();
+    defer wait_signal_mutex.unlock();
+    if (ms == 0) {
+        wait_signal_cond.wait(&wait_signal_mutex);
+    } else {
+        wait_signal_cond.timedWait(
+            &wait_signal_mutex,
+            ms * std.time.ns_per_ms,
+        ) catch |err| switch (err) {
+            error.Timeout => {},
+        };
+    }
+    return wait_signal;
+}
+
+fn logFn(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    std.debug.lockStdErr();
+    defer std.debug.unlockStdErr();
+    const stderr = std.io.getStdErr().writer();
+
+    var buf: [20]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buf);
+    const allocator = fba.allocator();
+    const time_str: []u8 = allocator.alloc(u8, buf.len) catch {
+        nosuspend stderr.print(
+            "failed to allocate memory to convert timestamp to string\n",
+            .{},
+        ) catch return;
+        return;
+    };
+    defer allocator.free(time_str);
+
+    const timestamp = time.time(null);
+    if (timestamp == -1) {
+        nosuspend stderr.print(
+            "failed to retrieve current time from time.h\n",
+            .{},
+        ) catch return;
+        return;
+    }
+    const tm_info = time.localtime(&timestamp);
+    const n = time.strftime(
+        time_str[0..buf.len],
+        buf.len,
+        "%Y-%m-%d %H:%M:%S",
+        tm_info,
+    );
+    // We need to compare with buf length - 1 because returning length
+    // doesn't contain terminating null character.
+    if (n != buf.len - 1) {
+        nosuspend stderr.print(
+            "failed to format current timestamp using time.h: {d}\n",
+            .{n},
+        ) catch return;
+        return;
+    }
+
+    const scoped_level = comptime switch (scope) {
+        .gpa => std.log.Level.debug,
+        else => level,
+    };
+    nosuspend stderr.print(
+        "{s} " ++ "[" ++ comptime scoped_level.asText() ++ "] " ++ "(" ++ @tagName(scope) ++ ") " ++ format ++ "\n",
+        .{time_str} ++ args,
+    ) catch return;
 }
